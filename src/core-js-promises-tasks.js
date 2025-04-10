868870bb40349eca2119e7158c03a1866c20e203
Marharyta <rita.shyshkavets@gmail.com>
2025-04-10 20:27:44 +0200

    feat: complete queuePromises implementation

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -139,8 +139,21 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  let resultString = '';
+
+  function newPromise(i) {
+    if (i >= promises.length) {
+      return Promise.resolve(resultString);
+    }
+
+    return promises[i].then((value) => {
+      resultString += value;
+      return newPromise(i + 1);
+    });
+  }
+
+  return newPromise(0);
 }
 
 module.exports = {
